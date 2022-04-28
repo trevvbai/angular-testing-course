65c1c4eb5699cc3f630bfbeca00e66578db7a2ac
trevbai <tbailey@lislv.com>
2022-04-28 17:02:38 -0500

    created test for add and test for subtract

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,20 @@
+﻿import {CalculatorService} from "./calculator.service";
+import {LoggerService} from "./logger.service";
+
+describe('CalculatorService', () => {
+
+  beforeEach(()=>{
+    const calculator = new CalculatorService(new LoggerService());
+  })
+  it('add two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.add(2,2);
+    expect(result).toBe(4);
+
+  })
+
+  it('subtract two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.subtract(2,2);
+    expect(result).toBe(0, "unexpected subtraction result");  })
+})
